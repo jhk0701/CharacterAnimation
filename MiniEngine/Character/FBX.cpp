64dcfd6aaf83835f18c7dfe91add162a6c73775f
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2026-07-01 11:09:14 +0900

    Feat: fbx sdk 라이브러리 추가

---
+++ MiniEngine/Character/FBX.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "FBX.h"
+#include "Utility.h"
+
+#include <fbxsdk.h>
+
+namespace FBX 
+{
+	bool Asset::Parse(const std::wstring& filePath) 
+	{
+
+		return true;
+	};
+
+	void Shutdown()
+	{
+	}
+}
---
+++ MiniEngine/Character/FBX.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
+namespace FBX
+{
+	struct Image 
+	{
+		std::wstring path;
+	};
+
+	struct Material
+	{
+        float baseColorFactor[4];    
+        float emissiveFactor[3];     
+        float normalTextureScale;    // Always 1.0 (FBX has no scale)
+        float metallicFactor;        
+        float roughnessFactor;       
+        uint32_t flags;              // PSOFlags bits : alphaTest, alphaBlend, twoSided
+        int baseColorTexIdx;         // -1 = none
+        int normalTexIdx;
+        int emissiveTexIdx;
+	};
+
+#pragma region 버텍스 및 메시 데이터
+
+    // Triangle 데이터 확장 -> 각 폴리곤 버텍스 정보 구조체
+    struct Primitive
+    {
+        std::vector<XMFLOAT3> positions;
+        std::vector<XMFLOAT3> normals;
+        std::vector<XMFLOAT4> tangents;
+        std::vector<XMFLOAT2> uv0;
+        std::vector<XMUINT4>  jointIndices;
+        std::vector<XMFLOAT4> jointWeights;
+        uint32_t vertexCount;
+        int materialIdx;
+        uint32_t attribMask;
+        bool hasSkin;
+        XMFLOAT3 minPos;
+        XMFLOAT3 maxPos;
+    };
+
+    struct Mesh
+    {
+        std::vector<Primitive> primitives; // 버텍스 정보
+        int skinIdx; // -1 : 스킨 없음
+    };
+
+    // FBX SDK 로드 호환용 Node
+    struct Node
+    {
+        std::wstring name;
+        int meshIdx;
+        int skinIdx;
+        std::vector<int> children;
+        int linearIdx;
+        bool skeletonRoot;
+
+        XMFLOAT3 scale;
+        XMFLOAT3 translation;
+        XMFLOAT3 rotation; // 쿼터니언 회전값
+    };
+
+    // 스키닝 데이터
+    struct Skin
+    {
+        std::vector<int> jointNodeIndices;
+        std::vector<XMFLOAT4X4> inverseBindMatrices;
+    };
+
+#pragma endregion
+
+#pragma region 애니메이션
+
+    struct AnimKey
+    {
+        float time;
+        float v[4]; // xyz : translation / scale, xyzw : rotation(quat)
+    };
+
+    struct AnimChannel
+    {
+        int targetNodeIdx;
+        int targetPath;
+        std::vector<AnimKey> keys;
+    };
+
+    struct AnimStack
+    {
+        std::wstring name;
+        float duration;
+    };
+
+#pragma endregion
+
+    // 로드된 모델 클래스
+    class Asset 
+    {
+    public:
+        Asset() {};
+        Asset(const std::wstring& filePath) { Parse(filePath); }
+    
+        bool Parse(const std::wstring& filePath);
+
+        std::wstring m_basePath;
+        std::vector<Image> m_imgs;
+        std::vector<Material> m_mats;
+        std::vector<Mesh> m_meshes;
+        std::vector<Node> m_nodes;
+        std::vector<Skin> m_skins;
+        std::vector<AnimStack> m_anims;
+        std::vector<int> m_rootNodes;
+    };
+
+    // FBX SDK : FbxManager* 리소스 반환용
+    void Shutdown();
+}
